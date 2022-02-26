d8a084ec258bb78b59c9104aaf5960beecd5f7bf
Zach <zach.laforge@hotmail.com>
2022-02-26 13:52:24 -0500

    finished if/else added comments

--- day_trip_generator.py
+++ day_trip_generator.py
@@ -7,35 +7,74 @@ transportations = ['Rocket', 'Broom', 'Teleport', 'Walk']
 entertainments = ['Movies', 'Biking', 'Shooting', 'People Watching']
 
 #User Story: Randomly select or re-select (four user stories)
-destination = random.choice(destinations)
-resturant= random.choice(resturants)
-transporation=random.choice(transportations)
-entertainment = random.choice(entertainments)
+#User Story: Function to have a single responsibility
 
-#User Story: Allow user to re-select ANY if they don't like
-#need a function to confirm, but has to pull in each of the chosen values from each list
-print(f'Is {destination} the choice you want? ')
-user_choice = int(input(f'Type 1 for yes or 2 for no '))
-    # if(user_choice == 1):
-    #     #lock in the first choice from random operation 
-    # elif(user_choice == 2):
-    #     #run the random operation again
-    # else:
-    #     #run the choice if/else again
+def random_choice(the_list):
+    potato = random.choice(the_list)
+    return potato
 
-#User Story: Confirm once user likes all options
+destination = random_choice(destinations)
+resturant= random_choice(resturants)
+transporation=random_choice(transportations)
+entertainment = random_choice(entertainments)
 
-#User Story: Display completed trip in console
-
+isFirst = True
 notOkay = True
 while notOkay:
-    
-    dest_choice = int(input(f'Type 1 for yes or 2 for no '))
-    if(user_choice == 2):
-        destination = random.choice(destination)
+    if isFirst:
+        print(f"""
+This is your current day trip
 
-    user_choice = int(input(f'Type 1 for yes or 2 for no '))
-    if(user_choice == 1):
+Destination: {destination}
+Resturant: {resturant}
+Transportation: {transporation}
+Entertainment: {entertainment}
+        """)
+        isFirst = False
+
+    change_or_not = input("""
+would you like to change anything?
+
+1. Destination
+2. Resturant
+3. Transportation
+4. Entertainment
+    """)
+
+    if change_or_not == '1':
+        destinations.remove(destination)
+        destination = random_choice(destinations)
+    elif change_or_not == '2':
+        resturants.remove(resturant)
+        resturant= random_choice(resturants)
+    elif change_or_not == '3':
+        transportations.remove(transporation)
+        transporation=random_choice(transportations)
+    elif change_or_not == '4':
+        entertainments.remove(entertainment)
+        entertainment = random_choice(entertainments)
+        
+
+    print(f"""
+This is your current day trip
+
+Destination: {destination}
+Resturant: {resturant}
+Transportation: {transporation}
+Entertainment: {entertainment}
+        """)
+#User Story: Confirm y/n if you're satisfied with your trip selections 
+    user_choice=input(f'Are you satisfiied with your current day trip? Y/n')
+    if(user_choice.lower() == 'y'):
         notOkay = False
-        #lock in the first choice from random operation 
- 
+
+#User Story: Display completed trip into the console
+
+        print(f"""
+        
+Enjoy your stay at {destination}  !
+I heard the food at {resturant} is wonderful !
+I hope {transporation} is as comfortable as possible !
+Have a great time during {entertainment}
+        
+        """)
